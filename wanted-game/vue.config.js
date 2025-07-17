098960f2f59ad69f7874c7ce7e885254e8d6cbd3
nfb1799 <nfb1799@rit.edu>
2025-07-17 11:11:25 -0400

    Title update

--- wanted-game/vue.config.js
+++ wanted-game/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: 'wanted-game',
+  publicPath: '/wanted-game/',
   transpileDependencies: true
 })
